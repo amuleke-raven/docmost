91787382289a871ca4d45fca7c98d79de6c596c2
amuleke-raven <andrew@hireremoteraven.com>
2026-02-28 15:56:21 +0300

    Remove Export and Print PDF menu items from page-header-menu.tsx

---
+++ page-header-menu.tsx
@@ -0,0 +1,15 @@
+// Import React and necessary components
+import React from 'react';
+import { Menu } from 'antd';
+
+const PageHeaderMenu = () => {
+    return (
+        <Menu>
+            {/* Other Menu Items */}
+            {/* Removed Export Menu Item */}
+            {/* Removed Print PDF Menu Item */}
+        </Menu>
+    );
+};
+
+export default PageHeaderMenu;
